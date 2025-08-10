0d0594f6d5998246d1dfffd54891f1acb7873dd2
Korodee <koroskki@gmail.com>
2025-08-10 14:51:14 +0100

    Fixed hero img

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -28,9 +28,7 @@ export default function Hero() {
       {/* Background Image with Enhanced Effects */}
       <div
         className="absolute inset-0 bg-cover bg-center z-0"
-        style={{ 
-          backgroundImage: "url('https://images.unsplash.com/photo-1549366021-9f761d450615?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')" 
-        }}
+        style={{ backgroundImage: "url('/hero-img.jpg')" }}
       />
       <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/70" />
 
