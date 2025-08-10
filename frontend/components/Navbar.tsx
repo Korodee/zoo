eed521d77dfe27c8d3b550a944bd4edc86ebbcfa
Korodee <koroskki@gmail.com>
2025-08-10 14:37:06 +0100

    Quick fixes

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -94,7 +94,7 @@ export default function Navbar() {
       id="navbar"
       className="fixed top-0 left-0 right-0 z-50 transition-all duration-300 bg-transparent"
     >
-      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-[83rem] mx-auto px-4 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16 md:h-16">
           {/* Left: logo */}
           <div className="flex items-center">
